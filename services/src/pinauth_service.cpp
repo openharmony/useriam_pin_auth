8629c5630cf47703af3e4fc8381b5dfd656641d2
韩震 <hanzhen10@huawei.com>
2022-03-01 11:50:24 +0800

    fix verify
    Signed-off-by: 韩震 <hanzhen10@huawei.com>
    Change-Id: I8b85a192c8e3f4233cf3c5168c079553ffb5fefd

--- services/src/pinauth_service.cpp
+++ services/src/pinauth_service.cpp
@@ -21,6 +21,7 @@
 #include "coauth_info_define.h"
 #include "accesstoken_kit.h"
 #include "pinauth_service.h"
+#include "useridm_client.h"
 
 namespace OHOS {
 namespace UserIAM {
@@ -156,6 +157,24 @@ void PinAuthService::ActuatorInfoQuery()
     AuthResPool::AuthExecutorRegistry::GetInstance().QueryStatus(*executor_, mngIQ_);
 }
 
+class ReconciliationCallback : public UserIDM::GetInfoCallback {
+public:
+    virtual ~ReconciliationCallback() = default;
+
+    void OnGetInfo(std::vector<UserIDM::CredentialInfo>& info) override;
+};
+
+void ReconciliationCallback::OnGetInfo(std::vector<UserIDM::CredentialInfo>& info)
+{
+    PINAUTH_HILOGI(MODULE_SERVICE, "PinAuthService::DoVerifyTemplateData enter");
+    std::vector<uint64_t> templateIdList;
+    for (uint32_t i = 0; i < info.size(); i++) {
+        templateIdList.push_back(info[i].templateId);
+    }
+    std::shared_ptr<PinAuth> pinHdi = std::make_shared<PinAuth>();
+    pinHdi->VerifyTemplateData(templateIdList);
+}
+
 void PinAuthService::OnResult(uint32_t resultCode)
 {
     PINAUTH_HILOGI(MODULE_SERVICE, "PinAuthService::OnResult enter");
@@ -164,6 +183,11 @@ void PinAuthService::OnResult(uint32_t resultCode)
         PINAUTH_HILOGI(MODULE_SERVICE, "PinAuthService::OnResult resultCode == SUCCESS");
         return;
     }
+    using namespace UserIDM;
+    std::shared_ptr<GetInfoCallback> reconciliationCallback =
+        std::make_shared<ReconciliationCallback>();
+    UserIDM::UserIDMClient::GetInstance().GetAuthInfo(UserIDMClient::ALL_INFO_GET_USER_ID, UserIDM::PIN,
+        reconciliationCallback);
     // To do register
     executorID_ = AuthResPool::AuthExecutorRegistry::GetInstance().Register(executor_, mngEx_);
     if (executorID_ == INVALID_EXECUTOR_ID) {
