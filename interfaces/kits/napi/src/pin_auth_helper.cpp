229a87e65f06b6fe84aacd14997c6cd695651a6a
caohongfei2 <caohongfei2@huawei.com>
2022-06-01 11:33:23 +0800

    fix:potential leak of memory point,passed-by-value struct argument contains uninitialized data
    Signed-off-by: caohongfei2 <caohongfei2@huawei.com>
    Change-Id: Iefc2b438c1fe1339a849e0cc4cad3674a9e79e99

--- interfaces/kits/napi/src/pin_auth_helper.cpp
+++ interfaces/kits/napi/src/pin_auth_helper.cpp
@@ -30,17 +30,18 @@ struct InputConstructorInfo {
 napi_value PinAuthServiceConstructor(napi_env env, napi_callback_info info)
 {
     PINAUTH_HILOGI(MODULE_JS_NAPI, "PinAuthHelper, PinAuthServiceConstructor start");
-    PinAuthImpl *pinAuthPtr = new (std::nothrow) PinAuthImpl();
+    std::unique_ptr<PinAuthImpl> pinAuthPtr {new (std::nothrow) PinAuthImpl()};
     if (pinAuthPtr == nullptr) {
         PINAUTH_HILOGE(MODULE_JS_NAPI, "%{public}s, get nullptr", __func__);
         return nullptr;
     }
+
     napi_value thisVar = nullptr;
     size_t argc = PIN_PARAMS_ONE;
     napi_value argv[PIN_PARAMS_ONE] = {nullptr};
     NAPI_CALL(env, napi_get_cb_info(env, info, &argc, argv, &thisVar, nullptr));
     NAPI_CALL(env, napi_wrap(
-        env, thisVar, pinAuthPtr,
+        env, thisVar, pinAuthPtr.get(),
         [](napi_env env, void *data, void *hint) {
             PinAuthImpl *pinAuthImpl = static_cast<PinAuthImpl *>(data);
             if (pinAuthImpl != nullptr) {
@@ -48,6 +49,7 @@ napi_value PinAuthServiceConstructor(napi_env env, napi_callback_info info)
             }
         },
         nullptr, nullptr));
+    pinAuthPtr.release();
     return thisVar;
 }
 
